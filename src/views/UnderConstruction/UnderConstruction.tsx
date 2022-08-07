8d3de75e7df9f35823bef833db718568bab62d58
Greg <gergodemeny@gmail.com>
2022-08-07 17:19:09 +0300

    Graph under the UnderConstruction page

--- src/views/UnderConstruction/UnderConstruction.tsx
+++ src/views/UnderConstruction/UnderConstruction.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import Graph from 'views/Graph/Graph';
 
 const BP1 = '@media (max-width: 260px)';
 
@@ -11,21 +12,39 @@ const sx = {
 		alignItems: 'center',
 		px: '16px',
 	},
+	textContainer: {
+		position: 'relative',
+		py: '12px',
+		px: '16px',
+		borderRadius: '10px',
+		backgroundColor: 'black',
+		cursor: 'default',
+	},
 	text: {
 		fontFamily: 'poppins',
 		fontWeight: 500,
 		fontSize: '24px',
 		textAlign: 'center',
 		transition: 'font-size .3s',
+		color: 'white',
 		[BP1]: {
 			fontSize: '18px',
 		},
+	},
+	graph: {
+		position: 'absolute',
+		height: '100vh',
+		top: 0,
+		left: 0,
 	},
 };
 
 const UnderConstruction = () => (
 	<Box sx={sx.root}>
-		<Typography sx={sx.text}>This site is under construction</Typography>
+		<Graph style={sx.graph} />
+		<Box sx={sx.textContainer}>
+			<Typography sx={sx.text}>This site is under construction</Typography>
+		</Box>
 	</Box>
 );
 
