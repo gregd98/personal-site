052ddce84ce22cf800331d61661e3ecd68043ff3
Greg <gergodemeny@gmail.com>
2022-08-15 15:07:13 +0300

    lodash replaced with lodash.debounce

--- src/hooks/useDebouncedResizeObserver.ts
+++ src/hooks/useDebouncedResizeObserver.ts
@@ -1,6 +1,6 @@
 import { useState, useMemo, RefObject } from 'react';
 import useResizeObserver from 'use-resize-observer';
-import _ from 'lodash';
+import debounce from 'lodash.debounce';
 
 interface ISize {
 	width?: number
@@ -9,7 +9,7 @@ interface ISize {
 
 const useDebouncedResizeObserver =  (ref: RefObject<any>, wait: number, leading = false): ISize => {
 	const [size, setSize] = useState<ISize>({});
-	const onResize = useMemo(() => _.debounce(setSize, wait, { leading }), [
+	const onResize = useMemo(() => debounce(setSize, wait, { leading }), [
 		wait, leading,
 	]);
 	useResizeObserver({ ref, onResize });
